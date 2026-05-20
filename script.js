14f9ab42190260fbc5bb0707dababa882799ac5d
acesavvy101 <gracielladeronne07@gmail.com>
2026-05-20 23:35:26 +0800

    feat: add basic arithmethic functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+console.log(add(1,2));
+
+function substract (num1,num2) {
+    return num1 - num2;
+}
+console.log(substract(4,9));
+
+function multiply (num1,num2) {
+    return (num1*num2);
+}
+console.log(multiply(6,3));
+
+function divide (num1,num2) {
+    return num1/num2;
+}
+console.log (divide(4,2));
